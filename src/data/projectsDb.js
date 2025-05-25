3cffa42e4ba9bab64af612cd4ee54b4db4638f57
Abhay <abhay.work.02@gmail.com>
2025-05-26 00:54:07 +0530

    Setting up the project.

---
+++ src/data/projectsDb.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        name: 'Mini Message Board',
+        githubLink: 'https://github.com/Abhay212121/mini-msg-board',
+        previewLink: 'https://mini-msg-board-9qhq.onrender.com/',
+        desc: 'A simple and user-friendly message board where users can post their thoughts and comments.',
+        techStackUsed: [
+            { name: 'NodeJS', logo: './images/techStack/nodejs.svg' },
+            { name: 'ExpressJS', logo: './images/techStack/express.svg' },
+            { name: 'PostgreSQL', logo: './images/techStack/sql.svg' },
+            { name: 'Tailwind', logo: './images/techStack/tailwind.svg' },
+            { name: 'git', logo: './images/techStack/git.svg' }
+        ]
+    }
+]
+
+export default projects
