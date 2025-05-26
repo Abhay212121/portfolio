e00eac1501df5147fc5d5ff5b4c4bba8ca648284
Abhay <abhay.work.02@gmail.com>
2025-05-26 21:29:43 +0530

    typo fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Footer from "./components/Footer";
 import ProjectSection from "./components/ProjectSection";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="bg-gradient-to-br from-[#152543] to-[#061432] py-22">
+    <div className="bg-[#061432] py-22">
       <Header />
       <ProjectSection />
       <Footer />
